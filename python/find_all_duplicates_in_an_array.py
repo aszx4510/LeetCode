7fc302b61e6fa901db48ec700fcdaa3491c359ae
aszx4510 <aszx4510@gmail.com>
2017-09-22 03:52:20 +0800

    Add two solutions

---
+++ python/find_all_duplicates_in_an_array.py
@@ -0,0 +1,20 @@
+
+# 442. Find All Duplicates in an Array
+
+# https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
+# http://www.geeksforgeeks.org/find-duplicates-in-on-time-and-constant-extra-space/
+
+
+class Solution(object):
+    def findDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        for x in nums:
+            if nums[abs(x) - 1] < 0:
+                res.append(abs(x))
+            else:
+                nums[abs(x) - 1] *= -1
+        return res
