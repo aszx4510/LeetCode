5ff39785d8731caf59fdb281e47b26683d0422dd
aszx4510 <aszx4510@gmail.com>
2017-09-27 01:22:04 +0800

    Add solution of #28

---
+++ python/implement_strstr().py
@@ -0,0 +1,17 @@
+
+# 28. Implement strStr()
+
+# https://leetcode.com/problems/implement-strstr/
+
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        try:
+            return haystack.index(needle)
+        except:
+            return -1
