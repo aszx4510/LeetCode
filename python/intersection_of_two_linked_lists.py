08921efd9ff0dad439fdf9da51b17b77f853e365
aszx4510 <aszx4510@gmail.com>
2017-10-05 03:14:22 +0800

    Add solution of #160

---
+++ python/intersection_of_two_linked_lists.py
@@ -0,0 +1,30 @@
+
+# 160. Intersection of Two Linked Lists
+
+# https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+# https://discuss.leetcode.com/topic/28067/java-solution-without-knowing-the-difference-in-len
+# Two interations, two pointer "must" meet at the intersection.
+# Second interation should change to the other linked list, and the path will be same.
+# If the two linked lists have no intersection, a and b will be None (tail node) simultaneously.
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        if headA is None and headB is None:
+            return None
+
+        a, b = headA, headB
+        while a is not b:
+            a = a.next if a is not None else headB
+            b = b.next if b is not None else headA            
+        return a
