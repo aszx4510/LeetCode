024dd7f0a303f3bbc40b3968f914dbd2929b6ce3
aszx4510 <aszx4510@gmail.com>
2017-10-17 05:37:05 +0800

    Create solution of #235

---
+++ python/lowest_common_ancestor_of_a_binary_search_tree.py
@@ -0,0 +1,35 @@
+
+# 235. Lowest Common Ancestor of a Binary Search Tree
+
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+# https://discuss.leetcode.com/topic/18387/3-lines-with-o-1-space-1-liners-alternatives
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        # for Binary Search Tree (BST)
+        while (root.val - p.val) * (root.val - q.val) > 0:
+            root = (root.left, root.right)[p.val > root.val]  # [p.val > root.val] will be the [0] or [1]
+        return root
+
+
+        # my method for any binary tree
+        # if root is p or root is q or root is None:
+        #     return root
+
+        # left_lca = self.lowestCommonAncestor(root.left, p, q)
+        # right_lca = self.lowestCommonAncestor(root.right, p, q)
+        # return root if left_lca and right_lca else left_lca or right_lca
