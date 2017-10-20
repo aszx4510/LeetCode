eb739a40bf575362a5ff723bbad1b00c3859164e
aszx4510 <aszx4510@gmail.com>
2017-10-21 04:20:11 +0800

    Create solution of #344

---
+++ python/reverse_string.py
@@ -0,0 +1,14 @@
+
+# 344. Reverse String
+
+# https://leetcode.com/problems/reverse-string/description/
+# http://yehnan.blogspot.tw/2015/04/python.html
+
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s[::-1]
