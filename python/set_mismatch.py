609ab9407127b4a5f8ade526f22028e66017b4ba
aszx4510 <aszx4510@gmail.com>
2017-11-16 02:33:54 +0800

    Create solution of #645

---
+++ python/set_mismatch.py
@@ -0,0 +1,32 @@
+
+# 645. Set Mismatch
+
+# https://leetcode.com/problems/set-mismatch/description/
+# https://discuss.leetcode.com/topic/96807/python-straightforward-with-explanation
+
+
+class Solution(object):
+    def findErrorNums(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # a little math
+        nums_sum = (len(nums) + 1) * len(nums) // 2
+        no_repeat_sum = sum(set(nums))
+        miss = nums_sum - no_repeat_sum
+        repeat = sum(nums) - no_repeat_sum
+        return repeat, miss
+
+
+        # straightforward
+        count = [0] * len(nums)
+        for num in nums:
+            count[num - 1] += 1
+        repeat, miss = -1, -1
+        for i in range(len(count)):
+            if count[i] > 1:  # count[i] == 2
+                repeat = i + 1
+            elif count[i] <= 0:  # count[i] == 0
+                miss = i + 1
+        return repeat, miss
