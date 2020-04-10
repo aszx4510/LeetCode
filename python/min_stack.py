45ca879d579eaa04188b15654195958418badc0d
aszx4510 <aszx4510@gmail.com>
2020-04-11 06:07:15 +0800

    Add some comments for more information

--- python/min_stack.py
+++ python/min_stack.py
@@ -51,3 +51,11 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.min_stack[-1]
+        
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
