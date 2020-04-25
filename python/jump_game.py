8b84c85b4160ac4951e4a2e87021649e886444dd
aszx4510 <aszx4510@gmail.com>
2020-04-26 02:10:13 +0800

    Add another solution of #55

--- python/jump_game.py
+++ python/jump_game.py
@@ -7,6 +7,15 @@
 
 class Solution(object):
     def canJump(self, nums: List[int]) -> bool:
+        # My new version code
+        max_jump, now = 0, 0
+        while now < len(nums) and now <= max_jump:
+            max_jump = max(max_jump, now + nums[now])
+            now += 1
+        return max_jump >= (len(nums) - 1)
+
+
+        # Old version
         if not nums:
             return True
         checked, far_index = 0, 0
