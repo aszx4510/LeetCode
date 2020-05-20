adfd5578a1469b02f733df13df8beb85b71c2a26
aszx4510 <aszx4510@gmail.com>
2020-05-21 04:25:41 +0800

    Remove redundant variable 'target'

--- python/kth_smallest_element_in_a_bst.py
+++ python/kth_smallest_element_in_a_bst.py
@@ -14,20 +14,20 @@
 class Solution:
     def kthSmallest(self, root: TreeNode, k: int) -> int:
         # My version
-        def dfs(node, current, target):
+        def dfs(node, current):
             if not node:
                 return current
-            left_result = dfs(node.left, current, target)
-            if left_result is None or current > target:
+            left_result = dfs(node.left, current)
+            if left_result is None or current > k:
                 return None
             current = left_result + 1
 
-            if current == target:
+            if current == k:
                 ans[0] = node.val
                 return None
             else:
-                return dfs(node.right, current, target)
+                return dfs(node.right, current)
 
         ans = [0]
-        dfs(root, 0, k)
+        dfs(root, 0)
         return ans[0]
