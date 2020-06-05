1296f09f67fd8e7e6c029fc8ba10375716b33897
aszx4510 <aszx4510@gmail.com>
2020-06-05 08:55:05 +0800

    Update the solution of #344, modify list in-place

--- python/reverse_string.py
+++ python/reverse_string.py
@@ -6,9 +6,12 @@
 
 
 class Solution(object):
-    def reverseString(self, s):
+    def reverseString(self, s: List[str]) -> None:
         """
-        :type s: str
-        :rtype: str
+        Do not return anything, modify s in-place instead.
         """
-        return s[::-1]
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
