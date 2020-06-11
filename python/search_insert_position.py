c48ffa96a6aa2548971bb35f78263ab39aaaacf8
aszx4510 <aszx4510@gmail.com>
2020-06-11 10:11:52 +0800

    Add new method for #35

--- python/search_insert_position.py
+++ python/search_insert_position.py
@@ -11,6 +11,21 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+        # Binary search, O(logn)
+        # According to the description: You may assume no duplicates in the array.
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
+
+
+        # My old method, O(n)
         for i in range(len(nums)):
             if target <= nums[i]:
                 return i
