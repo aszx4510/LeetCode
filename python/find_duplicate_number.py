3893b5b74137536ef617bd8b112318865233f7ef
aszx4510 <aszx4510@gmail.com>
2020-06-27 05:36:36 +0800

    Create solution of #287

---
+++ python/find_duplicate_number.py
@@ -0,0 +1,20 @@
+
+# 287. Find the Duplicate Number
+
+# https://leetcode.com/problems/find-the-duplicate-number/
+# https://leetcode.com/problems/find-the-duplicate-number/discuss/72846/My-easy-understood-solution-with-O(n)-time-and-O(1)-space-without-modifying-the-array.-With-clear-explanation.
+# https://leetcode.com/problems/find-the-duplicate-number/discuss/72846/My-easy-understood-solution-with-O(n)-time-and-O(1)-space-without-modifying-the-array.-With-clear-explanation./75491
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow, fast = nums[0], nums[nums[0]]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+        fast = 0
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        return slow
