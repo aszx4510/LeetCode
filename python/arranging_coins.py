210c270c1c7d0321d51545a1ea99c40843eaa427
aszx4510 <aszx4510@gmail.com>
2020-07-02 03:36:05 +0800

    Update solution of #441

--- python/arranging_coins.py
+++ python/arranging_coins.py
@@ -1,22 +1,31 @@
 
+
 # 441. Arranging Coins
 
 # https://leetcode.com/problems/arranging-coins/description/
-# https://discuss.leetcode.com/topic/65593/java-clean-code-with-explanations-and-running-time-2-solutions
+# https://leetcode.com/problems/arranging-coins/discuss/162814/Python-O(1)-Math-Solution-Fully-Explained
+# https://leetcode.com/problems/arranging-coins/discuss/92274/JAVA-Clean-Code-with-Explanations-and-Running-Time-2-Solutions
 
 
-class Solution(object):
-    def arrangeCoins(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        # math
-        import math
-        return int((-1 + math.sqrt(8 * n + 1)) / 2)
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        # Mathematics
+        return int((math.sqrt(8 * n + 1)-1)/2)
 
 
-        # my method, but O(log(n))
+        # Binary Search, O(log(n))
+        left, right = 0, n
+        while left <= right:
+            mid = (left + right) // 2
+
+            if (mid + 1) * mid <= 2 * n:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left - 1
+
+
+        # My old method, O(log(n))
         base = 1
         total = 0
         while total <= n:
