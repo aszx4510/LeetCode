440024fb6fd4cb2237e084972073bbc030cf068d
aszx4510 <aszx4510@gmail.com>
2020-07-14 03:28:57 +0800

    Modify the type hint for #100

--- python/same_tree.py
+++ python/same_tree.py
@@ -13,12 +13,7 @@
 #         self.right = None
 
 class Solution(object):
-    def isSameTree(self, p, q):
-        """
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: bool
-        """
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
         if p and q:
             return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         return p is q  # True for both p and q are None
