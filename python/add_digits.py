f7a6e25ef0da899b36108be20610cde5a44716ba
aszx4510 <aszx4510@gmail.com>
2020-07-27 04:03:52 +0800

    Modify the type hint for #258

--- python/add_digits.py
+++ python/add_digits.py
@@ -3,16 +3,13 @@
 
 # https://leetcode.com/problems/add-digits/description/
 # https://discuss.leetcode.com/topic/28791/3-methods-for-python-with-explains
-# N % 9 = a[0] + a[1] + ..a[n]
-# 9 % 9 = 0, so we can make (n - 1) % 9 + 1
 
 
-class Solution(object):
-    def addDigits(self, num):
-        """
-        :type num: int
-        :rtype: int
-        """
+class Solution:
+    def addDigits(self, num: int) -> int:
         if num == 0:
             return num
+        
+        # N % 9 = a[0] + a[1] + ..a[n]
+        # 9 % 9 = 0, so we can make (n - 1) % 9 + 1
         return ((num - 1) % 9) + 1
