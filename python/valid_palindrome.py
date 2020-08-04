a90239f6116e1b43083b9ad51a3e08748d5f3e5d
aszx4510 <aszx4510@gmail.com>
2020-08-04 14:00:00 +0800

    Modify the type hint of #125

--- python/valid_palindrome.py
+++ python/valid_palindrome.py
@@ -5,17 +5,13 @@
 # https://leetcode.com/problems/valid-palindrome/discuss/
 
 
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
         left, right = 0, len(s) - 1
-        while right > left:
-            while right > left and not s[left].isalnum():
+        while left < right:
+            while left < right and not s[left].isalnum():
                 left += 1
-            while right > left and not s[right].isalnum():
+            while left < right and not s[right].isalnum():
                 right -= 1
             if s[left].lower() != s[right].lower():
                 return False
