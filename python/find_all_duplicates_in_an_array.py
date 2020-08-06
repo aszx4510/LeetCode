a38cf9ee881769a10f1606c63ad6f0dc8399fd8f
aszx4510 <aszx4510@gmail.com>
2020-08-07 03:09:59 +0800

    Update solution of #442

--- python/find_all_duplicates_in_an_array.py
+++ python/find_all_duplicates_in_an_array.py
@@ -5,16 +5,12 @@
 # http://www.geeksforgeeks.org/find-duplicates-in-on-time-and-constant-extra-space/
 
 
-class Solution(object):
-    def findDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        res = []
-        for x in nums:
-            if nums[abs(x) - 1] < 0:
-                res.append(abs(x))
-            else:
-                nums[abs(x) - 1] *= -1
-        return res
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        result = []
+        for num in nums:
+            idx = abs(num) - 1
+            if nums[idx] < 0:
+                result.append(idx + 1)
+            nums[idx] *= -1
+        return
