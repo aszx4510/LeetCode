787c3f05de403a4312828a4d7231957f18a796a2
aszx4510 <aszx4510@gmail.com>
2020-08-11 11:18:12 +0800

    Modify the type hint of #171

--- python/excel_sheet_column_number.py
+++ python/excel_sheet_column_number.py
@@ -4,13 +4,9 @@
 # https://leetcode.com/problems/excel-sheet-column-number/description/
 
 
-class Solution(object):
-    def titleToNumber(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        number = 0
-        for i, ch in enumerate(reversed(s)):
-            number += (ord(ch) - ord('A') + 1) * (26 ** i)
-        return number
+class Solution:
+    def titleToNumber(self, s: str) -> int:
+        result = 0
+        for i, c in enumerate(reversed(s)):
+            result += (ord(c) - ord('A') + 1) * pow(26, i)
+        return result
