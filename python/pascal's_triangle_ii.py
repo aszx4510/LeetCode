e88e28c336a70354635fd955828adcb42bc192e1
aszx4510 <aszx4510@gmail.com>
2020-08-13 01:47:38 +0800

    Update solution of #119

--- python/pascal's_triangle_ii.py
+++ python/pascal's_triangle_ii.py
@@ -5,13 +5,18 @@
 # https://discuss.leetcode.com/topic/15559/very-simple-python-solution
 
 
-class Solution(object):
-    def getRow(self, rowIndex):
-        """
-        :type rowIndex: int
-        :rtype: List[int]
-        """
-        row = [1]
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        result = [0] * (rowIndex + 1)
+        result[0] += 1
+        for i in range(rowIndex + 1):
+            for j in range(i, 0, -1):
+                result[j] += result[j - 1]
+        return result
+
+
+        # solve by using list comprehension but not O(k) extra space
+        result = [1]
         for i in range(rowIndex):
-            row = [x + y for x, y in zip([0] + row, row + [0])]
-        return row
+            result = [x + y for x, y in zip([0] + result, result + [0])]
+        return result
