72bb4bd2af15feac476de9c5ecdc88ef2e921669
aszx4510 <aszx4510@gmail.com>
2020-08-13 01:48:34 +0800

    Modify the type hint of #118

--- python/pascal's_triangle.py
+++ python/pascal's_triangle.py
@@ -4,12 +4,8 @@
 # https://leetcode.com/problems/pascals-triangle/description/
 
 
-class Solution(object):
-    def generate(self, numRows):
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
         if numRows == 0:
             return []
         elif numRows == 1:
