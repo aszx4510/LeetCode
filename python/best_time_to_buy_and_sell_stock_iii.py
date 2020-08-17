1e2aed7f75eb4d24b37fc6089f30eeeee973d8e1
aszx4510 <aszx4510@gmail.com>
2020-08-18 03:46:21 +0800

    Add another method to solve #123

--- python/best_time_to_buy_and_sell_stock_iii.py
+++ python/best_time_to_buy_and_sell_stock_iii.py
@@ -3,10 +3,12 @@
 
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/discuss/39611/Is-it-Best-Solution-with-O(n)-O(1).
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/discuss/135704/Detail-explanation-of-DP-solution
 
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # Magic
         first_buy, second_buy = float('inf'), float('inf')
         first_profit, total_profit = 0, 0
 
@@ -20,3 +22,17 @@ class Solution:
             # total_profit = sell_2 - (buy_2 - (sell_1 - buy_1))
             total_profit = max(x - second_buy, total_profit)
         return total_profit
+
+
+        # Dynamic Programming, but TLE (Time Limit Exceeded)
+        # dp[k, i] = max(dp[k, i-1], prices[i] - prices[j] + dp[k-1, j-1]), j=[0..i-1]
+        if not prices:
+            return 0
+        dp = [[0] * len(prices) for _ in range(3)] # 3 kinds of transaction (0, 1, 2)
+        for k in range(1, 3):
+            for i in range(len(prices)):
+                minimum = prices[0]
+                for j in range(1, i):
+                    minimum = min(minimum, prices[j] - dp[k - 1][j - 1])
+                dp[k][i] = max(dp[k][i - 1], prices[i] - minimum)
+        return dp[-1][-1]
