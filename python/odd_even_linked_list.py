6eb29c64013d2971b5b4f0ee44562cfb6f91f44e
aszx4510 <aszx4510@gmail.com>
2021-12-02 17:04:25 +0800

    feat: revise solution of #328

--- python/odd_even_linked_list.py
+++ python/odd_even_linked_list.py
@@ -4,37 +4,47 @@
 # https://leetcode.com/problems/odd-even-linked-list/
 
 
-class Solution(object):
-    def oddEvenList(self, head: ListNode) -> ListNode:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # Concise version
         odds = odds_curr = ListNode(None)
         evens = evens_curr = ListNode(None)
         while head:
-            evens_curr.next = head
-            odds_curr.next = head.next
+            odds_curr.next = head
+            evens_curr.next = head.next
 
-            evens_curr = evens_curr.next
             odds_curr = odds_curr.next
+            evens_curr = evens_curr.next
 
-            head = head.next.next if odds_curr else None
-        evens_curr.next = odds.next
-        return evens.next
+            head = head.next.next if evens_curr else None
+        
+        odds_curr.next = evens.next
+        return odds.next
 
 
-        # Another version        
+        # My method
         if not head:
             return head
-        result = head
-        odds = odds_curr = ListNode(None)
-        while head and head.next:
-            odds_curr.next = head.next
-            odds_curr = odds_curr.next
+            
+        even_head = head.next
+        odd_curr, even_curr = head, even_head
 
-            head.next = head.next.next
-            prev = head
-            head = head.next
-        if not head:
-            head = prev
-        head.next = odds.next
-        odds_curr.next = None
-        return result
+        while odd_curr and even_curr:
+            odd_curr.next = odd_curr.next.next if odd_curr.next else None
+            even_curr.next = even_curr.next.next if even_curr.next else None
+
+            odd_prev, odd_curr = odd_curr, odd_curr.next
+            even_curr = even_curr.next
+
+        if odd_curr:
+            odd_curr.next = even_head
+        else:
+            odd_prev.next = even_head
+
+        return head
