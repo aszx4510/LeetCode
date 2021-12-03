ca257415634c3045b22953ad6227c6a1a23aef51
aszx4510 <aszx4510@gmail.com>
2021-12-03 14:10:58 +0800

    feat: add another solution for #167

--- python/two_sum_ii-input_array_is_sorted.py
+++ python/two_sum_ii-input_array_is_sorted.py
@@ -4,19 +4,16 @@
 # https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
 
 
-class Solution(object):
-    def twoSum(self, numbers, target):
-        """
-        :type numbers: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left, right = 0, len(numbers) - 1
         while left < right:
-            diff = target - numbers[left]
-            while left < right and numbers[right] >= diff:
-                if numbers[right] == diff:
-                    return [left + 1, right + 1]
+            temp = numbers[left] + numbers[right]
+            
+            if temp == target:
+                return [left + 1, right + 1]
+            elif temp < target:
+                left += 1
+            else:
                 right -= 1
-            left += 1
-        return None
+        return
