0e07377011dd8c7b69952427c810a39d1657c8a7
aszx4510 <aszx4510@gmail.com>
2021-12-07 05:18:14 +0800

    feat: revise solution of #695

--- python/0695-max_area_of_island.py
+++ python/0695-max_area_of_island.py
@@ -5,20 +5,13 @@
 # https://discuss.leetcode.com/topic/106370/easy-python
 
 
-class Solution(object):
-    def maxAreaOfIsland(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        if not grid:
-            return 0
-        m, n = len(grid), len(grid[0])
-
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         def dfs(i, j):
-            if 0 <= i < m and 0 <= j < n and grid[i][j]:
+            if 0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j]:
                 grid[i][j] = 0
-                return 1 + dfs(i - 1, j) + dfs(i + 1, j) + dfs(i, j - 1) + dfs(i, j + 1)
+                return 1 + sum([dfs(i+x, j+y) for x, y in ((1, 0), (-1, 0), (0, 1), (0, -1))])
             return 0
-        areas = [dfs(i, j) for i in range(m) for j in range(n) if grid[i][j]]
-        return max(areas) if areas else 0
+        
+        areas = [dfs(i, j) for i in range(len(grid)) for j in range(len(grid[0]))]
+        return max(areas)
