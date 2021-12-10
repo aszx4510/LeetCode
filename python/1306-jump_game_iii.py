3aa022c7a1275c4de04048afaa6c2f349d3b579a
aszx4510 <aszx4510@gmail.com>
2021-12-10 12:50:50 +0800

    feat: add another version for #1306

--- python/1306-jump_game_iii.py
+++ python/1306-jump_game_iii.py
@@ -6,6 +6,26 @@
 
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
+        # Concise version
+        n = len(arr)
+        visited = [False] * n
+        queue = [start]
+
+        while queue:
+            idx = queue.pop(0)
+            if not (0 <= idx < n) or visited[idx]:
+                continue
+            elif arr[idx] == 0:
+                return True
+
+            visited[idx] = True
+            queue.append(idx + arr[idx])
+            queue.append(idx - arr[idx])
+
+        return False
+
+
+        # My old answer
         queue, arrived = [start], set([start])
         while queue:
             now = queue.pop(0)
