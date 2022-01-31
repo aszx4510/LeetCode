7c1c3a385749681829ae93fa89285e2f237ec0f2
aszx4510 <aszx4510@gmail.com>
2022-02-01 05:40:23 +0800

    feat: create solution of #1672

---
+++ python/1672-richest_customers_wealth.py
@@ -0,0 +1,9 @@
+
+# 1672. Richest Customer Wealth
+
+# https://leetcode.com/problems/richest-customer-wealth/
+
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        return max(sum(acc) for acc in accounts)
