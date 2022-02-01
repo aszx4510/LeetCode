0041ba24340780a3fb2b26575897cd20f3d0b999
aszx4510 <aszx4510@gmail.com>
2022-02-02 05:11:29 +0800

    feat: add another solution of #121

--- python/0121-best_time_to_buy_and_sell_stock.py
+++ python/0121-best_time_to_buy_and_sell_stock.py
@@ -5,29 +5,19 @@
 # https://discuss.leetcode.com/topic/19853/kadane-s-algorithm-since-no-one-has-mentioned-about-this-so-far-in-case-if-interviewer-twists-the-input
 
 
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # My new method
+        max_profit, min_cost = 0, prices[0]
+        for p in prices[1:]:
+            max_profit = max(p - min_cost, max_profit)
+            min_cost = min(p, min_cost)
+        return max_profit
+
+
         max_current, profit = 0, 0
         for i in range(1, len(prices)):
             # record the difference between each element
             max_current = max(0, max_current + (prices[i] - prices[i - 1]))
             profit = max(profit, max_current)
         return profit
-
-
-        # if not prices:
-        #     return 0
-        # minimum, maximum = prices[0], prices[0]
-        # profit = 0
-        # for price in prices:
-        #     if price < minimum:
-        #         minimum = price
-        #         maximum = 0
-        #     if price > maximum:
-        #         maximum = price
-        #         profit = maximum - minimum if maximum - minimum > profit else profit
-        # return profit
