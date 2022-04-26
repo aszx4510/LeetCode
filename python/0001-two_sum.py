e8ffa0af623d4edd32afad60a107ffb5ac332f56
aszx4510 <aszx4510@gmail.com>
2022-04-26 11:33:44 +0800

    feat: modify the solution of #1

--- python/0001-two_sum.py
+++ python/0001-two_sum.py
@@ -4,16 +4,11 @@
 # https://leetcode.com/problems/two-sum/description/
 
 
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        buff_dict = {}
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        remain = {}
         for i in range(len(nums)):
-            if nums[i] in buff_dict:
-                return [buff_dict[nums[i]], i]
+            if nums[i] in remain:
+                return [remain[nums[i]], i]
             else:
-                buff_dict[target - nums[i]] = i
+                remain[target - nums[i]] = i
