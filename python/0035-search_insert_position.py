d5fe3c97816946f98130d328b124b5d7dc9e48b7
aszx4510 <aszx4510@gmail.com>
2022-05-06 19:42:39 +0800

    feat: modify solution of #35

--- python/0035-search_insert_position.py
+++ python/0035-search_insert_position.py
@@ -3,22 +3,14 @@
 
 # https://leetcode.com/problems/search-insert-position/description/
 
-
-class Solution(object):
-    def searchInsert(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
         # Binary search, O(logn)
         # According to the description: You may assume no duplicates in the array.
         left, right = 0, len(nums) - 1
         while left <= right:
             mid = (left + right) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
+            if nums[mid] < target:
                 left = mid + 1
             else:
                 right = mid - 1
