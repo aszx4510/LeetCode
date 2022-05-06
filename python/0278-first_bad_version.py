d924abf0002e0f7f1ac70cb588f2dd4c84cfde20
aszx4510 <aszx4510@gmail.com>
2022-05-06 19:38:44 +0800

    feat: modify the solution of #278

--- python/0278-first_bad_version.py
+++ python/0278-first_bad_version.py
@@ -5,12 +5,11 @@
 # https://leetcode.com/problems/first-bad-version/discuss/71333/1-liner-in-Ruby-Python/203147
 
 
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
 class Solution:
-    def firstBadVersion(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+    def firstBadVersion(self, n: int) -> int:
         left, right = 1, n
         while left < right:
             mid = (left + right) // 2
