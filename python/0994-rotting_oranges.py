500a0c375a019c9d378ae5f7f4319254c3ffbce9
aszx4510 <aszx4510@gmail.com>
2022-05-24 23:07:15 +0800

    feat: modify solution of #994

--- python/0994-rotting_oranges.py
+++ python/0994-rotting_oranges.py
@@ -6,35 +6,29 @@
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        n, m = len(grid), len(grid[0])
-        fresh = 0
-        queue = []
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == 1:
-                    fresh += 1
-                elif grid[i][j] == 2:
+        m, n = len(grid), len(grid[0])
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        queue = deque([])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 2:
                     queue.append((i, j))
 
-        def rot(i, j):
-            if not (0 <= i < n and 0 <= j < m):
-                return
-            if grid[i][j] == 1 and (i, j) not in queue:
-                queue.append((i, j))
+        step = 2
+        while queue:
+            i, j = queue.popleft()
+            for d in range(len(dirs)):
+                ni, nj = i + dirs[d][0], j + dirs[d][1]
+                if not (0 <= ni < m and 0 <= nj < n):
+                    continue
+                if grid[ni][nj] == 1:
+                    grid[ni][nj] = grid[i][j] + 1
+                    step = max(step, grid[ni][nj])
+                    queue.append((ni, nj))
 
-        direction = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        step = -1
-        while queue and fresh:
-            temp_queue = queue.copy()
-            queue.clear()
-            step += 1
-            while temp_queue:
-                i, j = temp_queue.pop(0)
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] == 1:
-                    grid[i][j] = 2
-                    fresh -= 1
-                for x, y in direction:
-                    rot(i + x, j + y)
-
-        # Avoid there are all rotten oranges in the original grid
-        return max(0, step) if fresh == 0 else -1
+                    return -1
+        return step - 2
